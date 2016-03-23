aa205fd8fbd1575aae9561797ccd1804e7b7fba3
llluchko <l.mladenov21@gmail.com>
2016-03-23 04:44:57 +0200

    feat: Implement base user interface
    More detailed explanatory text:
    
    -created views for Login with Udacity profile,
    TabBar for switching between map with users
    and Map View with testing users on the map
    
    -added image assets in vector format
    
    -added some constants and GCD functionality

---
+++ OnTheMap/LoginViewController.swift
@@ -0,0 +1,197 @@
+//
+//  LoginViewController.swift
+//  MyFavoriteMovies
+//
+//  Created by Jarrod Parkes on 1/23/15.
+//  Copyright (c) 2015 Udacity. All rights reserved.
+//
+
+import UIKit
+
+// MARK: - LoginViewController: UIViewController
+
+class LoginViewController: UIViewController {
+    
+    // MARK: Properties
+    
+    var appDelegate: AppDelegate!
+    var keyboardOnScreen = false
+    
+    // MARK: Outlets
+    
+    @IBOutlet weak var usernameTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    @IBOutlet weak var loginButton: BorderedButton!
+    @IBOutlet weak var debugTextLabel: UILabel!
+    
+    // MARK: Life Cycle
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // get the app delegate
+        appDelegate = UIApplication.sharedApplication().delegate as! AppDelegate                        
+        
+        configureUI()
+        
+        subscribeToNotification(UIKeyboardWillShowNotification, selector: Constants.Selectors.KeyboardWillShow)
+        subscribeToNotification(UIKeyboardWillHideNotification, selector: Constants.Selectors.KeyboardWillHide)
+        subscribeToNotification(UIKeyboardDidShowNotification, selector: Constants.Selectors.KeyboardDidShow)
+        subscribeToNotification(UIKeyboardDidHideNotification, selector: Constants.Selectors.KeyboardDidHide)
+    }
+    
+    override func viewWillDisappear(animated: Bool) {
+        super.viewWillDisappear(animated)
+        unsubscribeFromAllNotifications()
+    }
+    
+    // MARK: Login
+    
+    @IBAction func loginPressed(sender: AnyObject) {
+        
+        let controller = self.storyboard!.instantiateViewControllerWithIdentifier("TabBarController") as! UITabBarController
+        self.presentViewController(controller, animated: true, completion: nil)
+        
+//        userDidTapView(self)
+//        
+//        if usernameTextField.text!.isEmpty || passwordTextField.text!.isEmpty {
+//            debugTextLabel.text = "Username or Password Empty."
+//        } else {
+//            setUIEnabled(false)
+//            
+//            /*
+//                Steps for Authentication...
+//                https://www.themoviedb.org/documentation/api/sessions
+//                
+//                Step 1: Create a request token
+//                Step 2: Ask the user for permission via the API ("login")
+//                Step 3: Create a session ID
+//                
+//                Extra Steps...
+//                Step 4: Get the user id ;)
+//                Step 5: Go to the next view!            
+//            */
+//          //  getRequestToken()
+//        }
+    }
+    
+    private func completeLogin() {
+        performUIUpdatesOnMain {
+            self.debugTextLabel.text = ""
+            self.setUIEnabled(true)
+            let controller = self.storyboard!.instantiateViewControllerWithIdentifier("MoviesTabBarController") as! UITabBarController
+            self.presentViewController(controller, animated: true, completion: nil)
+        }
+    }
+    
+
+}
+
+// MARK: - LoginViewController: UITextFieldDelegate
+
+extension LoginViewController: UITextFieldDelegate {
+    
+    // MARK: UITextFieldDelegate
+    
+    func textFieldShouldReturn(textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return true
+    }
+    
+    // MARK: Show/Hide Keyboard
+    
+    func keyboardWillShow(notification: NSNotification) {
+        if !keyboardOnScreen {
+            view.frame.origin.y -= keyboardHeight(notification)
+        }
+    }
+    
+    func keyboardWillHide(notification: NSNotification) {
+        if keyboardOnScreen {
+            view.frame.origin.y += keyboardHeight(notification)
+        }
+    }
+    
+    func keyboardDidShow(notification: NSNotification) {
+        keyboardOnScreen = true
+    }
+    
+    func keyboardDidHide(notification: NSNotification) {
+        keyboardOnScreen = false
+    }
+    
+    private func keyboardHeight(notification: NSNotification) -> CGFloat {
+        let userInfo = notification.userInfo
+        let keyboardSize = userInfo![UIKeyboardFrameEndUserInfoKey] as! NSValue
+        return keyboardSize.CGRectValue().height
+    }
+    
+    private func resignIfFirstResponder(textField: UITextField) {
+        if textField.isFirstResponder() {
+            textField.resignFirstResponder()
+        }
+    }
+    
+    @IBAction func userDidTapView(sender: AnyObject) {
+        resignIfFirstResponder(usernameTextField)
+        resignIfFirstResponder(passwordTextField)
+    }
+}
+
+// MARK: - LoginViewController (Configure UI)
+
+extension LoginViewController {
+    
+    private func setUIEnabled(enabled: Bool) {
+        usernameTextField.enabled = enabled
+        passwordTextField.enabled = enabled
+        loginButton.enabled = enabled
+        debugTextLabel.text = ""
+        debugTextLabel.enabled = enabled
+        
+        // adjust login button alpha
+        if enabled {
+            loginButton.alpha = 1.0
+        } else {
+            loginButton.alpha = 0.5
+        }
+    }
+    
+    private func configureUI() {
+        
+        // configure background gradient
+        let backgroundGradient = CAGradientLayer()
+        backgroundGradient.colors = [Constants.UI.LoginColorTop, Constants.UI.LoginColorBottom]
+        backgroundGradient.locations = [0.0, 1.0]
+        backgroundGradient.frame = view.frame
+        view.layer.insertSublayer(backgroundGradient, atIndex: 0)
+        
+        configureTextField(usernameTextField)
+        configureTextField(passwordTextField)
+    }
+    
+    private func configureTextField(textField: UITextField) {
+        let textFieldPaddingViewFrame = CGRectMake(0.0, 0.0, 13.0, 0.0)
+        let textFieldPaddingView = UIView(frame: textFieldPaddingViewFrame)
+        textField.leftView = textFieldPaddingView
+        textField.leftViewMode = .Always
+        textField.backgroundColor = Constants.UI.GreyColor
+        textField.textColor = Constants.UI.BlueColor
+        textField.attributedPlaceholder = NSAttributedString(string: textField.placeholder!, attributes: [NSForegroundColorAttributeName: UIColor.whiteColor()])
+        textField.tintColor = Constants.UI.BlueColor
+        textField.delegate = self
+    }
+}
+
+// MARK: - LoginViewController (Notifications)
+
+extension LoginViewController {
+    
+    private func subscribeToNotification(notification: String, selector: Selector) {
+        NSNotificationCenter.defaultCenter().addObserver(self, selector: selector, name: notification, object: nil)
+    }
+    
+    private func unsubscribeFromAllNotifications() {
+        NSNotificationCenter.defaultCenter().removeObserver(self)
+    }
+}
--- OnTheMap/SecondViewController.swift
+++ OnTheMap/SecondViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class SecondViewController: UIViewController {
+class SecondViewController: UITableViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
